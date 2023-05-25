b4a6c2ab4e0663880cb2b9d4b7dd449929cd7364
Лёвин Николай Сергеевич <151705@grpz.su>
2023-05-25 16:15:25 +0300

    update create new user without picture

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import router from "./router.js"
+import router from './router.js'
 import fileUpload from 'express-fileupload'
 
 const PORT = 5000
@@ -9,21 +9,19 @@ const DB_URL = 'mongodb://localhost:27017/alphabet'
 
 const app = express()
 
-app.use(express.json())             //нужно тк express по дефолту не понимает json
-app.use(express.static('static'))   // нужно чтобы сервер мог отдавать статические файлы, например изображения
-app.use(fileUpload({}))             //подключаем миделвеер fileUpload для работы с файлами, мы будем с фото
+app.use(express.json()) //нужно тк express по дефолту не понимает json
+app.use(express.static('static')) // нужно чтобы сервер мог отдавать статические файлы, например изображения
+app.use(fileUpload({})) //подключаем миделвеер fileUpload для работы с файлами, мы будем с фото
 
 app.use('/api', router)
 
-
-
-async function startApp(){
-    try{
-        await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
         app.listen(PORT, () => console.log('SERVER STARTED ON PORT' + PORT))
-    }catch(e){
-        console.log(e);
+    } catch (e) {
+        console.log(e)
     }
 }
 
-startApp()  
+startApp()
--- router.js
+++ router.js
@@ -1,11 +1,10 @@
-import { Router } from "express";
+import { Router } from 'express'
 import Post from './Post.js'
-import PostController from "./PostController.js";
+import PostController from './PostController.js'
 
 const router = new Router()
 
 router.post('/letters', PostController.create)
-
 router.get('/letters', PostController.getAll)
 router.get('/letters/:id', PostController.getOne)
 router.put('/letters', PostController.update)
